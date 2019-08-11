6cfa1f9cdeaa5cb89749146e0969275a119d0962
Alex Grönholm <alex.gronholm@nextday.fi>
2019-08-11 14:24:07 +0300

    Revert "Removed a few obsolete workarounds"
    This reverts commit f7d69222

--- typeguard/__init__.py
+++ typeguard/__init__.py
@@ -405,7 +405,7 @@ origin_type_checkers = {
     type: check_class,
     Union: check_union
 }
-
+_subclass_check_unions = hasattr(Union, '__union_set_params__')
 if Type is not None:
     origin_type_checkers[Type] = check_class
 if Literal is not None:
@@ -442,8 +442,13 @@ def check_type(argname: str, value, expected_type, memo: Optional[_CallMemo] = N
     elif isclass(expected_type):
         if issubclass(expected_type, Tuple):
             check_tuple(argname, value, expected_type, memo)
+        elif issubclass(expected_type, Callable) and hasattr(expected_type, '__args__'):
+            # Needed on Python 3.5.0 to 3.5.2
+            check_callable(argname, value, expected_type, memo)
         elif issubclass(expected_type, (float, complex)):
             check_number(argname, value, expected_type)
+        elif _subclass_check_unions and issubclass(expected_type, Union):
+            check_union(argname, value, expected_type, memo)
         elif isinstance(expected_type, TypeVar):
             check_typevar(argname, value, expected_type, memo)
         elif issubclass(expected_type, IO):
