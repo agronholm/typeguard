fef8b475ce0a4bf88fff17ecf29b170fcd196225
Alex Grönholm <alex.gronholm@nextday.fi>
2020-11-21 11:48:27 +0200

    Fixed indentation

--- typeguard/__init__.py
+++ typeguard/__init__.py
@@ -882,7 +882,7 @@ def typechecked(func=None, *, always=False, _localns: Optional[Dict[str, Any]] =
                         property_func, always=always, _localns=func.__dict__
                     )
 
-               setattr(func, key, property(**kwargs))
+                setattr(func, key, property(**kwargs))
 
         return func
 
