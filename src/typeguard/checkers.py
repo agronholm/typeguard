0fbb5b8f8c7b24361a6289122d5c8e87ba3b10ef
Alex Grönholm <alex.gronholm@nextday.fi>
2021-12-01 00:37:52 +0200

    Pass origin_type and type arguments separately to type checkers

--- src/typeguard/checkers.py
+++ src/typeguard/checkers.py
@@ -87,17 +87,18 @@ BINARY_MAGIC_METHODS = {
 }
 
 
-def check_callable(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_callable(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                   memo: TypeCheckMemo) -> None:
     if not callable(value):
         raise TypeCheckError('is not callable')
 
-    if getattr(annotation, "__args__", None):
+    if args:
         try:
             signature = inspect.signature(value)
         except (TypeError, ValueError):
             return
 
-        argument_types = annotation.__args__[:-1]
+        argument_types = args[0]
         if argument_types != (Ellipsis,):
             # The callable must not have keyword-only arguments without defaults
             unfulfilled_kwonlyargs = [
@@ -125,26 +126,25 @@ def check_callable(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
                     f'but {num_mandatory_args} argument(s) declared')
 
 
-def check_dict(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_dict(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     if not isinstance(value, dict):
         raise TypeCheckError('value is not a dict')
 
-    if annotation is not dict:
-        if (hasattr(annotation, "__args__") and
-                annotation.__args__ not in (None, annotation.__parameters__)):
-            key_type, value_type = annotation.__args__
-            if key_type is not Any or value_type is not Any:
-                for k, v in value.items():
-                    check_type_internal(k, key_type, memo)
-                    check_type_internal(v, value_type, memo)
+    if args:
+        key_type, value_type = args
+        if key_type is not Any or value_type is not Any:
+            for k, v in value.items():
+                check_type_internal(k, key_type, memo)
+                check_type_internal(v, value_type, memo)
 
 
-def check_typed_dict(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
-    declared_keys = frozenset(annotation.__annotations__)
-    if hasattr(annotation, '__required_keys__'):
-        required_keys = annotation.__required_keys__
+def check_typed_dict(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                     memo: TypeCheckMemo) -> None:
+    declared_keys = frozenset(origin_type.__annotations__)
+    if hasattr(origin_type, '__required_keys__'):
+        required_keys = origin_type.__required_keys__
     else:  # py3.8 and lower
-        required_keys = declared_keys if annotation.__total__ else frozenset()
+        required_keys = declared_keys if origin_type.__total__ else frozenset()
 
     existing_keys = frozenset(value)
     extra_keys = existing_keys - declared_keys
@@ -157,65 +157,49 @@ def check_typed_dict(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
         keys_formatted = ', '.join('"{}"'.format(key) for key in sorted(missing_keys))
         raise TypeCheckError(f'is missing required key(s): {keys_formatted}')
 
-    for key, argtype in get_type_hints(annotation).items():
+    for key, argtype in get_type_hints(origin_type).items():
         argvalue = value.get(key, _missing)
         if argvalue is not _missing:
             check_type_internal(argvalue, argtype, memo)
 
 
-def check_list(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_list(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     if not isinstance(value, list):
         raise TypeCheckError('is not a list')
 
-    if annotation is not list:
-        args = get_args(annotation)
-        if args != annotation.__parameters__:
-            value_type = args[0]
-            if value_type is not Any:
-                for i, v in enumerate(value):
-                    check_type_internal(v, value_type, memo)
+    if args and args != (Any,):
+        for i, v in enumerate(value):
+            check_type_internal(v, args[0], memo)
 
 
-def check_sequence(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_sequence(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                   memo: TypeCheckMemo) -> None:
     if not isinstance(value, collections.abc.Sequence):
         raise TypeCheckError('is not a sequence')
 
-    args = get_args(annotation)
-    if args != annotation.__parameters__:
-        value_type = args[0]
-        if value_type is not Any:
-            for i, v in enumerate(value):
-                check_type_internal(v, value_type, memo)
+    if args and args != (Any,):
+        for i, v in enumerate(value):
+            check_type_internal(v, args[0], memo)
 
 
-def check_set(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_set(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     if not isinstance(value, AbstractSet):
         raise TypeCheckError('is not a set')
 
-    if annotation is not set:
-        if hasattr(annotation, "__args__") and annotation.__args__ not in \
-                (None, annotation.__parameters__):
-            value_type = annotation.__args__[0]
-            if value_type is not Any:
-                for v in value:
-                    check_type_internal(v, value_type, memo)
+    if args and args != (Any,):
+        for v in value:
+            check_type_internal(v, args[0], memo)
 
 
-def check_tuple(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_tuple(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     # Specialized check for NamedTuples
-    if sys.version_info >= (3, 8):
-        is_named_tuple = hasattr(annotation, '__annotations__')
-    else:
-        is_named_tuple = hasattr(annotation, '_field_types')  # deprecated since python 3.8
+    field_types = getattr(origin_type, '__annotations__', None)
+    if field_types is None and sys.version_info < (3, 8):
+        field_types = getattr(origin_type, '_field_types', None)
 
-    if is_named_tuple:
-        if not isinstance(value, annotation):
-            raise TypeCheckError(f'is not a named tuple of type {qualified_name(annotation)}')
-
-        if sys.version_info >= (3, 8):
-            field_types = annotation.__annotations__
-        else:
-            field_types = annotation._field_types
+    if field_types:
+        if not isinstance(value, origin_type):
+            raise TypeCheckError(f'is not a named tuple of type {qualified_name(origin_type)}')
 
         for name, field_type in field_types.items():
             check_type_internal(getattr(value, name), field_type, memo)
@@ -224,7 +208,6 @@ def check_tuple(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
     elif not isinstance(value, tuple):
         raise TypeCheckError('is not a tuple')
 
-    args = get_args(annotation)
     if args:
         # Python 3.6+
         use_ellipsis = args[-1] is Ellipsis
@@ -250,9 +233,9 @@ def check_tuple(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
             check_type_internal(element, element_type, memo)
 
 
-def check_union(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_union(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     errors: Dict[str, TypeCheckError] = {}
-    for type_ in get_args(annotation):
+    for type_ in args:
         try:
             check_type_internal(value, type_, memo)
             return
@@ -263,17 +246,17 @@ def check_union(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
     raise TypeCheckError(f'did not match any element in the union:\n{formatted_errors}')
 
 
-def check_class(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_class(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     if not isclass(value):
         raise TypeCheckError('is not a class')
 
     # Needed on Python 3.7+
-    if annotation is Type or annotation is type:
+    if not args:
         return
 
-    expected_class = get_args(annotation)[0]
+    expected_class = args[0]
     if isinstance(expected_class, TypeVar):
-        check_typevar(value, expected_class, memo, subclass_check=True)
+        check_typevar(value, expected_class, (), memo, subclass_check=True)
     elif get_origin(expected_class) is Union:
         errors: Dict[str, TypeCheckError] = {}
         for arg in get_args(expected_class):
@@ -281,7 +264,7 @@ def check_class(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
                 return
 
             try:
-                check_class(value, Type[arg], memo)
+                check_class(value, type, (arg,), memo)
                 return
             except TypeCheckError as exc:
                 errors[get_type_name(arg)] = exc
@@ -293,25 +276,26 @@ def check_class(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
         raise TypeCheckError(f'is not a subclass of {qualified_name(expected_class)}')
 
 
-def check_newtype(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
-    supertype = annotation.__supertype__
+def check_newtype(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                  memo: TypeCheckMemo) -> None:
+    supertype = origin_type.__supertype__
     if not isinstance(value, supertype):
         raise TypeCheckError(f'is not an instance of {qualified_name(supertype)}')
 
 
-def check_instance(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
-    origin_type = get_origin(annotation) or annotation
+def check_instance(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                   memo: TypeCheckMemo) -> None:
     if not isinstance(value, origin_type):
         raise TypeCheckError(f'is not an instance of {qualified_name(origin_type)}')
 
 
-def check_typevar(value: Any, typevar: TypeVar, memo: TypeCheckMemo, *,
+def check_typevar(value: Any, origin_type: TypeVar, args: Tuple[Any, ...], memo: TypeCheckMemo, *,
                   subclass_check: bool = False) -> None:
-    if typevar.__bound__ is not None:
-        annotation = Type[typevar.__bound__] if subclass_check else typevar.__bound__
+    if origin_type.__bound__ is not None:
+        annotation = Type[origin_type.__bound__] if subclass_check else origin_type.__bound__
         check_type_internal(value, annotation, memo)
-    elif typevar.__constraints__:
-        for constraint in typevar.__constraints__:
+    elif origin_type.__constraints__:
+        for constraint in origin_type.__constraints__:
             annotation = Type[constraint] if subclass_check else constraint
             try:
                 check_type_internal(value, annotation, memo)
@@ -321,41 +305,40 @@ def check_typevar(value: Any, typevar: TypeVar, memo: TypeCheckMemo, *,
                 break
         else:
             formatted_constraints = ', '.join(get_type_name(constraint)
-                                              for constraint in typevar.__constraints__)
+                                              for constraint in origin_type.__constraints__)
             raise TypeCheckError(f'does not match any of the constraints '
                                  f'({formatted_constraints})')
 
 
-def check_literal(value: Any, expected_type: Any, memo: TypeCheckMemo) -> None:
-    def get_literal_args(literal):
+def check_literal(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                  memo: TypeCheckMemo) -> None:
+    def get_literal_args(literal_args: Tuple[Any, ...]) -> Tuple[Any, ...]:
         retval = []
-        for arg in get_args(literal):
+        for arg in literal_args:
             if get_origin(arg) is Literal:
                 # The first check works on py3.6 and lower, the second one on py3.7+
-                retval.extend(get_literal_args(arg))
+                retval.extend(get_literal_args(arg.__args__))
             elif arg is None or isinstance(arg, (int, str, bytes, bool, Enum)):
                 retval.append(arg)
             else:
                 raise TypeError(f'Illegal literal value: {arg}')  # TypeError here is deliberate
 
-        return retval
+        return tuple(retval)
 
-    final_args = tuple(get_literal_args(expected_type))
+    final_args = tuple(get_literal_args(args))
     if value not in final_args:
         formatted_args = ', '.join(repr(arg) for arg in final_args)
         raise TypeCheckError(f'is not any of ({formatted_args})')
 
 
-def check_number(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
-    if annotation is complex and not isinstance(value, (complex, float, int)):
+def check_number(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
+    if origin_type is complex and not isinstance(value, (complex, float, int)):
         raise TypeCheckError('is neither complex, float or int')
-    elif annotation is float and not isinstance(value, (float, int)):
+    elif origin_type is float and not isinstance(value, (float, int)):
         raise TypeCheckError('is neither float or int')
 
 
-def check_io(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
-    origin_type = get_origin(annotation) or annotation
-    args = get_args(annotation)
+def check_io(value: Any, origin_type: Any, args: Tuple[Any, ...], memo: TypeCheckMemo) -> None:
     if origin_type is TextIO or (origin_type is IO and args == (str,)):
         if not isinstance(value, TextIOBase):
             raise TypeCheckError('is not a text based I/O object')
@@ -366,20 +349,22 @@ def check_io(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
         raise TypeCheckError('is not an I/O object')
 
 
-def check_protocol(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_protocol(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                   memo: TypeCheckMemo) -> None:
     # TODO: implement proper compatibility checking and support non-runtime protocols
-    if getattr(annotation, '_is_runtime_protocol', False):
-        if not isinstance(value, annotation):
-            raise TypeCheckError(f'is not compatible with the {annotation.__qualname__} protocol')
+    if getattr(origin_type, '_is_runtime_protocol', False):
+        if not isinstance(value, origin_type):
+            raise TypeCheckError(f'is not compatible with the {origin_type.__qualname__} protocol')
 
 
-def check_byteslike(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
+def check_byteslike(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                    memo: TypeCheckMemo) -> None:
     if not isinstance(value, (bytearray, bytes, memoryview)):
         raise TypeCheckError('is not bytes-like')
 
 
-def check_instanceof(value: Any, annotation: Any, memo: TypeCheckMemo) -> None:
-    origin_type = get_origin(annotation) or annotation
+def check_instanceof(value: Any, origin_type: Any, args: Tuple[Any, ...],
+                     memo: TypeCheckMemo) -> None:
     if not isinstance(value, origin_type):
         raise TypeCheckError(f'is not an instance of {qualified_name(origin_type)}')
 
@@ -400,13 +385,13 @@ def check_type_internal(value: Any, annotation: Any, memo: TypeCheckMemo) -> Non
 
     origin_type = get_origin(annotation)
     if origin_type is Annotated:
-        annotation, *extras = annotation.__args__
+        annotation, *extras = get_args(annotation)
         origin_type = get_origin(annotation)
     else:
         extras = ()
 
     if origin_type is not None:
-        args = annotation.__args__
+        args = get_args(annotation)
     else:
         origin_type = annotation
         args = ()
@@ -414,7 +399,7 @@ def check_type_internal(value: Any, annotation: Any, memo: TypeCheckMemo) -> Non
     for lookup_func in config._config.checker_lookup_functions:
         checker = lookup_func(origin_type, args, extras)
         if checker:
-            checker(value, annotation, memo)
+            checker(value, origin_type, args, memo)
             return
 
     if not isinstance(value, origin_type):
