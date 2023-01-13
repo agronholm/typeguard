d1f2d86157047e33fad71f37ea84d1935e0c5926
jacobpbrugh <65986207+jacobpbrugh@users.noreply.github.com>
2023-01-13 12:42:22 -0500

    Make @typechecked decorator work when @static/classmethod decorators are used any order (#254)
    Co-authored-by: Alex Grönholm <alex.gronholm@nextday.fi>
    
    Fixes #231.

--- src/typeguard/__init__.py
+++ src/typeguard/__init__.py
@@ -227,6 +227,12 @@ def typechecked(func=None, *, always=False, _localns: Optional[Dict[str, Any]] =
     if not __debug__ and not always:  # pragma: no cover
         return func
 
+    if isinstance(func, (classmethod, staticmethod)):
+        if isinstance(func, classmethod):
+            return classmethod(typechecked(func.__func__))
+        else:
+            return staticmethod(typechecked(func.__func__))
+
     if isclass(func):
         prefix = func.__qualname__ + "."
         for key, attr in func.__dict__.items():
